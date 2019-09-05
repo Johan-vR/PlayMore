9c2282a2819613edf07bcc9050e4e7682e143741
Johan van Rooyen <johan@thumbzup.com>
2019-09-05 10:13:06 +0200

    Added count function

--- src/PlayMore.cpp
+++ src/PlayMore.cpp
@@ -15,10 +15,16 @@ void subProc(int a, int b)
 	printf("a+b=%d",a+b);
 }
 
+void count(int a, int b)
+{
+	for (int i=0; i < b; i++)
+		printf("%d\n",a+i);
+}
 
 int main() {
 	printf("Hello world!\n");
 	printf("a+b=%d\n", subProcAdd(3,5));
 	printf("a-b=%d\n", subProcAdd(8,3));
+	count(10,10);
 	return 0;
 }
