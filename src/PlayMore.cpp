a1169c400ca74aa3545c7582e370621d599f4653
Johan van Rooyen <johan@thumbzup.com>
2019-09-05 09:50:33 +0200

    Update PlayMore.cpp

--- src/PlayMore.cpp
+++ src/PlayMore.cpp
@@ -11,12 +11,18 @@ using namespace std;
 
 void subProc(int a, int b)
 {
-	printf("a+b=%d",a+b);
+	printf("a+b=%d\n",a+b);
+}
+
+void subInc(int a)
+{
+	printf("a++=%d,%d\n",++a,a++);
 }
 
 
 int main() {
 	printf("Hello world!\n");
 	subProc(3,5);
+	subInc(9);
 	return 0;
 }
