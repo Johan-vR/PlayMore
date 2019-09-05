ad60d80e8cc264a14847e1e6d2773e275ed079c3
Johan van Rooyen <johan@thumbzup.com>
2019-09-05 08:58:24 +0200

    Update calc.cpp and calc.h

---
+++ src/calc.cpp
@@ -0,0 +1,18 @@
+/*
+ * calc.cpp
+ *
+ *  Created on: 05 Sep 2019
+ *      Author: johanvr
+ */
+
+
+int subProcAdd(int a, int b)
+{
+	return a+b;
+}
+
+int subProcSub(int a, int b)
+{
+	return a-b;
+}
+
---
+++ src/calc.h
@@ -0,0 +1,15 @@
+/*
+ * calc.h
+ *
+ *  Created on: 05 Sep 2019
+ *      Author: johanvr
+ */
+
+#ifndef CALC_H_
+#define CALC_H_
+
+int subProcAdd(int a, int b);
+int subProcSub(int a, int b);
+
+
+#endif /* CALC_H_ */
